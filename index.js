20cee9afbca31126a5cb5b73d876ebe8b8ad6e21
zeeshujunaid <zeeshan.junaid.developer@gmail.com>
2024-05-01 16:09:00 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// 1.Fibonacci Sequence: Write a function to generate the Fibonacci sequence up to a given number n
+// var a = 0;
+// var b = 1;
+
+// for (let i = 0; i <= 10; i++) {
+//     var temp = a + b;
+//     console.log(temp);
+//     a = b;
+//     b = temp; 
+// }
+
+// 2.Factorial Calculation: Write a function to calculate the factorial of a given number
+
+// var number = prompt("please enter your number");
+// var fact = 1;
+// if (number == 0) {
+//     console.log(`the factorial of ${number} is 1`);
+// }else if (number < 0){
+//     console.log(`the factorial of -negative number is not possible`);
+// }else{
+//     for (var i = 1; i <= number; i++) {
+//             fact = fact * i;
+//             console.log(`the factorial of is ${fact}`);
+// }
+//  }
+
+// 3.Reverse a String: Write a function to reverse a string without using the built-in reverse() function.
+
+
